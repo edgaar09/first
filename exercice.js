63d3f058ae7af1bf8b4db516a5552f0eda811b0a
edgaar09 <edgar.valls.salla@gmail.com>
2022-07-01 16:42:18 +0200

    array elements printed

---
+++ exercice.js
@@ -0,0 +1,27 @@
+const array1 = ["Hello", "World", "in", "a", "frame"];
+
+
+for (string in array1){
+    console.log(`*${array1[string]}*`)
+}
+
+
+/*
+const lenght = array1.length;
+
+if (array1.lenght = 0){
+    console.log("There are no elements in the array")
+}else {
+    for (element in array1){
+        console.log("*")
+    }
+}
+
+
+console.log(array1)
+*/
+
+
+
+
+
