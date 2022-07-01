64a9a1b847e4e8cb34219c43f303ad1f83d1417f
Ramon Salla <rsallar@gmail.com>
2022-07-01 21:07:04 +0200

    Output to html

--- exercise.js
+++ exercise.js
@@ -1,20 +1,21 @@
 const array1 = ["Hello", "World", "in", "a", "frame"];
 
-console.log(array1)
+const breakLine = "<br>";
+let output = "";
 
 array1.push('element1')
 array1.push('element2')
 
 
 for (element in array1){
-    console.log('*')
+    output +='*';
 }
-
+output += breakLine;
 array1.pop()
 array1.pop()
 
 for (string in array1) {
-    console.log(`*${array1[string]}*`)
+    output += array1[string] + breakLine;
 }
 
 array1.push('element1')
@@ -22,5 +23,7 @@ array1.push('element2')
 
 
 for (element in array1){
-    console.log('*')
+    output += "*";
 }
+
+document.getElementById("log").innerHTML=output;
