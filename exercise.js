98842fa286000ac64768d8a56e1f2680d597a15f
Ramon Salla <rsallar@gmail.com>
2022-07-03 20:09:41 +0200

    formatted + added button

--- exercise.js
+++ exercise.js
@@ -1,22 +1,24 @@
 const array = ["Hello", "World", "in", "a", "frame"];
 
-const breakLine = "<br>";
-let output = "";
+function printBox() {
+    const breakLine = "<br>";
+    let output = "";
 
-let maxChar = 0;
+    let maxChar = 0;
 
-for(let i=0; i<array.length; i++){
-    if(maxChar<array[i].length){
-        maxChar = array[i].length
+    for (let i = 0; i < array.length; i++) {
+        if (maxChar < array[i].length) {
+            maxChar = array[i].length
+        }
     }
+
+    output += "*".repeat(maxChar + 2) + breakLine;
+
+    for (let i = 0; i < array.length; i++) {
+        output += "*" + array[i] + " ".repeat(maxChar - array[i].length) + "*" + breakLine;
+    }
+    output += "*".repeat(maxChar + 2) + breakLine;
+
+    document.getElementById("log").innerHTML = output;
 }
-
-output += "*".repeat(maxChar+2) + breakLine;
-
-for(let i = 0; i<array.length; i++){
-    output += "*"+array[i]+" ".repeat(maxChar-array[i].length) + "*"+breakLine;
-}
-output += "*".repeat(maxChar+2) + breakLine;
-
-document.getElementById("log").innerHTML=output;
 
