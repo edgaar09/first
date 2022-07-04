7952c43f76b0c84e018acec489eee6d817451334
Ramon Salla <rsallar@gmail.com>
2022-07-04 13:23:35 +0200

    added exercise2 and debug config

---
+++ main.js
@@ -0,0 +1,11 @@
+function exercise2(){
+    console.log("a\nb");
+    console.log("Test  !");
+    let counter=0;
+    for (let i=0; i<5; i++){
+        counter = "iteration_"+i;
+    }
+
+}
+
+exercise2(); //execute
